9d4ff84c97c400228030b2e1880529cfd4c59fde
Mohamed Soliman <prackels@gmail.com>
2023-10-19 15:33:07 +0300

    1.3

--- blog/project/blog/urls.py
+++ blog/project/blog/urls.py
@@ -4,7 +4,5 @@ app_name = 'blog'
 urlpatterns = [
 # Post views
 path('', views.PostListView.as_view(), name='post_list'),
-path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-views.post_detail,
-name='post_detail'),
+path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
