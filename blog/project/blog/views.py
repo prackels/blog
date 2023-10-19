e872bbeb12aab25d5c95c129558248288e417a69
Mohamed Soliman <prackels@gmail.com>
2023-10-19 04:15:33 +0300

    1.2

--- blog/project/blog/views.py
+++ blog/project/blog/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator 
 from .models import Post
-def post_list(request):
-    post = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post, 3) # The def Name (Viwe Name) & objs Number in one page
-    page_number = request.GET.get("page")
-    posts = paginator.get_page(page_number) # 
-    return render(request, 'blog/post/list.html', {'posts': posts}) # posts the context of pagination
+from django.views.generic import ListView, DetailView
+# def post_list(request):
+#     post = Post.published.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post, 3) # The def Name (Viwe Name) & objs Number in one page
+#     page_number = request.GET.get("page")
+#     posts = paginator.get_page(page_number) # 
+#     return render(request, 'blog/post/list.html', {'posts': posts}) # posts the context of pagination
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+
+
+
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
